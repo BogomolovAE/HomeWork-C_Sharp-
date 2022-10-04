8365c4915ff1e7f8632b801bcb1d1209d4806766
Alexander Bogomolov <a.e.bogomolov@gmail.com>
2022-10-04 20:11:06 +0300

    Task_4 Done

---
+++ Task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+int index=0;
+int [] myArray = new int [3];
+int max=0;
+while (index<3)
+{
+    Console.WriteLine("Insert number № "+(index+1));
+    myArray[index]=Convert.ToInt32(Console.ReadLine());
+    if (index==0) max=myArray[index];
+    else if (myArray[index]>max) max=myArray[index];
+    index++;
+}
+Console.WriteLine ("Maximum is: "+max);
