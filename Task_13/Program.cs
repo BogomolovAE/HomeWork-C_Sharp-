09537a50213589bf0d61864473fd1741579a2f4f
Alexander Bogomolov <a.e.bogomolov@gmail.com>
2022-10-07 10:20:19 +0300

    Task_13 and Task_15 Check procedure added

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -5,16 +5,23 @@
 78 -> третьей цифры нет
 
 32679 -> 6 */
-m1:
 Console.Write("Input number: ");
 string N=Console.ReadLine();
-try{                                
-Convert.ToInt32(N);                 //Блок проверки ввода числа а не строки
-}
-catch 
+string Check (string N)                                                         // Процедура проверки значений введенной строки
 {
-    Console.WriteLine ("You String is not a number");
-    goto m1;
+    m1:
+    try{                                
+        Convert.ToInt32(N);                                                      //Блок проверки ввода числа а не строки
+    }
+    catch 
+    {
+        Console.Write ("You String is not a number, try again: ");
+        N=Console.ReadLine();
+        goto m1;
+    }
+    N=Convert.ToString(Math.Abs(Convert.ToInt32(N)));                           //Убираем нули и минусы вначале числа
+    return N;
 }
+N=Check(N);
 if (N.Length<3) Console.Write("There is no third digit in your number");
 else Console.WriteLine ("The third digit of number N is: "+N[2]);
--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -3,29 +3,29 @@
 6 -> да
 7 -> да
 1 -> нет */
-m1:
+
 Console.Write("Input number of week day (1..7): ");
 string N=Console.ReadLine();
-try{                                
-Convert.ToInt32(N);                 //Блок проверки ввода числа а не строки
-}
-catch 
+string Check (string N) // Процедура проверки значений введенной строки
 {
-    Console.WriteLine ("You String is not a number, try again");
-    goto m1;
+    m1:
+    try{                                
+        Convert.ToInt32(N);                 //Блок проверки ввода числа а не строки
+    }
+    catch 
+    {
+        Console.Write ("You String is not a number, try again: ");
+        N=Console.ReadLine();
+        goto m1;
+    }
+    return N;
 }
-while (Convert.ToInt32(N)<1||Convert.ToInt32(N)>8)
+
+while (Convert.ToInt32(N)<1||Convert.ToInt32(N)>7)
 {
     Console.Write("There is no such day in week, try again (number must be between 1 and 7): ");
     N=Console.ReadLine();
-    try{                                
-Convert.ToInt32(N);                 //Блок проверки повторного ввода числа а не строки
-}
-catch 
-{
-    Console.WriteLine ("You String is not a number, try again");
-    goto m1;
-}
+    N=Check(N);
 }
 if (Convert.ToInt32(N)<6) Console.WriteLine (N + " is a weekday");
 else Console.WriteLine (N + " is a weekend");
