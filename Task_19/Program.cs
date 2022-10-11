d7c421ca6febec3bd29d08cfbf442c27c1ef1da9
Alexander Bogomolov <a.e.bogomolov@gmail.com>
2022-10-11 18:41:06 +0300

    Task_19 in 2 variants Done

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -3,9 +3,9 @@
 12821 -> да
 23432 -> да */
 //                              VARIANT 1 (WHITH LONGINT)
-Console.WriteLine("Enter a number: ");
+/* Console.WriteLine("Enter a number: ");
 string numberInString=Console.ReadLine();
- string Check (string N)                                         // Процедура проверки значений введенной строки
+string Check (string N)                                         // Процедура проверки значений введенной строки
 {
     m1:
     while (N=="")
@@ -40,5 +40,45 @@ while (number/10>0)
     if (number%10!=(number/tenPower)) break;
     number=(number-tenPower*(number/tenPower)-number%10)/10;
    }
-if (number==0||number/10==0) Console.WriteLine("Polynome");
-else Console.WriteLine("Not polynome");
+if (number==0||number/10==0) Console.WriteLine("Number is the polynomial");
+else Console.WriteLine("Number is not the polynomial"); */
+ //                              VARIANT 2 (WHITH STRING)
+ Console.WriteLine("Enter a number: ");
+string numberInString=Check(Console.ReadLine());
+string Check (string N)                                         // Процедура проверки значений введенной строки
+{
+    m1:
+    while (N=="")
+    {
+        Console.Write("Your String is empty, try again: ");     // Проверка ввода пустой строки
+        N=Console.ReadLine();
+    }
+  
+    try{                                
+        Convert.ToInt64(N);                                     //Блок проверки ввода строки а не числа
+    }
+    catch (FormatException)
+    {
+        Console.Write ("You String is not a number, try again: ");
+        N=Console.ReadLine();
+        goto m1;
+    }
+    catch (OverflowException)                                   //Блок проверки переполнения типа int при конвертировании строки
+    
+    {
+      Console.Write ("You number, try again (number must be between –9 223 372 036 854 775 808 and 9 223 372 036 854 775 807): ");  
+      N=Console.ReadLine();
+      goto m1;
+    }
+    return N;
+}
+for (int i = 0; i<=numberInString.Length/2-1; i++)
+{
+    if (numberInString[i]!=numberInString[numberInString.Length-i-1])
+    
+    {
+        Console.WriteLine("Number is not the polynomial");
+        break;
+    }
+    if (i==numberInString.Length/2-1) Console.WriteLine("Number is the polynomial");
+}
