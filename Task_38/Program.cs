6bc14ab04b046237c0bfe26893e7a26c5d29c143
Alexander Bogomolov <a.e.bogomolov@gmail.com>
2022-10-18 20:53:23 +0300

    Task_38 Done

---
+++ Task_38/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */
+
+bool newMin=true;
+bool newMax=true;
+double maxVal=0;
+double minVal=0;
+double [] RandomArray(int numberOfElements,int min, int max)
+{
+    double [] arrayInDbl = new double [new Random().Next(1,numberOfElements)];
+    
+    for (int i=0;i<arrayInDbl.Length;i++)
+    {
+        arrayInDbl[i]=new Random().Next(min,max);
+        if (newMax) 
+        {
+            maxVal=arrayInDbl[i];
+            newMax=false;
+        }
+        else if (maxVal<arrayInDbl[i]) maxVal=arrayInDbl[i];
+        if (newMin) 
+        {
+            minVal=arrayInDbl[i];
+            newMin=false;
+        }
+        else if (minVal>arrayInDbl[i]) minVal=arrayInDbl[i];
+        Console.Write($"{arrayInDbl[i]}"+new string(' ',2));
+    }
+    return arrayInDbl;
+}
+RandomArray(20,-100, 100);
+Console.WriteLine();
+Console.WriteLine($"min={minVal}    max={maxVal}    max-min={maxVal-minVal}");
