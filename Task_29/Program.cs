a5e31eceb881d957ccb8de9dcac0fc350cf3e312
Alexander Bogomolov <a.e.bogomolov@gmail.com>
2022-10-18 19:39:36 +0300

    Task_29 Done

---
+++ Task_29/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив произвольной длины, заполняет произвольными элементами и выводит их на экран.
+Длину массива и элементы массива можно задать рандомно или попросить пользователя ввести в консоли.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] */
+
+int [] arrayInInt = new int [new Random().Next(1,10)];
+bool maxNew=true;
+int max=0;
+for (int i=0;i<arrayInInt.Length;i++)
+{
+    arrayInInt[i]=new Random().Next(-1000,1000);
+    if (maxNew)
+    {
+        max=arrayInInt[i];
+        maxNew=false;
+    }
+}
+    for (int i=0;i<arrayInInt.Length;i++)
+    {
+        Console.Write($"{arrayInInt[i]}"+new string(' ',2));
+    }
+
+ 
