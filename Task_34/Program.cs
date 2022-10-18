5bd78e556e148680959f34082519c8b6ac005ea4
Alexander Bogomolov <a.e.bogomolov@gmail.com>
2022-10-18 20:26:20 +0300

    Task_34 Done with function

--- Task_34/Program.cs
+++ Task_34/Program.cs
@@ -2,15 +2,20 @@
 
 [345, 897, 568, 234] -> 2
  */
-
-int [] arrayInInt = new int [new Random().Next(1,10)];
 int count=0;
-for (int i=0;i<arrayInInt.Length;i++)
-{
-    arrayInInt[i]=new Random().Next(100,999);
-    if (arrayInInt[i]%2==0) count ++;
-    Console.Write($"{arrayInInt[i]}"+new string(' ',2));
-}
+
+RandomArray(10,100,999);
 Console.WriteLine();
 Console.WriteLine($" There are {count} even numbers in array");
-   
+int [] RandomArray(int numberOfElements,int min, int max)
+{
+    int [] arrayInInt = new int [new Random().Next(1,numberOfElements)];
+    
+    for (int i=0;i<arrayInInt.Length;i++)
+    {
+        arrayInInt[i]=new Random().Next(min,max);
+        if (arrayInInt[i]%2==0) count++;
+        Console.Write($"{arrayInInt[i]}"+new string(' ',2));
+    }
+    return arrayInInt;
+}   
